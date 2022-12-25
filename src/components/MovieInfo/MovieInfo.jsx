f44f4b277fb89d7945fa7f6d2cdd808fafda1bb3
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-12-25 19:05:59 +0200

    refactoring

---
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from 'react-router-dom';
+export const MovieInfo = ({ movie }) => {
+  const imageUrl = 'https://image.tmdb.org/t/p/w500/';
+  return (
+    <>
+      <img
+        src={
+          movie.poster_path
+            ? `${imageUrl}${movie.poster_path}`
+            : 'https://e7.pngegg.com/pngimages/84/530/png-clipart-film-clapperboard-computer-icons-cinema-shows-miscellaneous-angle.png'
+        }
+        alt={movie.title}
+        width="300"
+      />
+      <h1>{movie.title}</h1>
+      <p>User Score: {movie.vote_average * 10}%</p>
+      <h2>Overview</h2>
+      <p>{movie.overview}</p>
+      {<h3>Genres</h3>}
+      {<p>{movie.genres.map(genre => genre.name).join(' ')}</p>}
+      <h3>Additional information</h3>
+      <ul>
+        <li>
+          <NavLink to="cast">Cast</NavLink>
+        </li>
+        <li>
+          <NavLink to="reviews">Reviews</NavLink>
+        </li>
+      </ul>
+    </>
+  );
+};
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,9 @@
 import { ButtonGoBack } from 'components/ButtonGoBack/ButtonGoBack';
+import { MovieInfo } from 'components/MovieInfo/MovieInfo';
 import { useState, useEffect } from 'react';
-import { NavLink, Outlet, useParams, useLocation } from 'react-router-dom';
+import { Outlet, useParams, useLocation } from 'react-router-dom';
 import { fetchMoviesById } from 'services/fetchApi';
-const imageUrl = 'https://image.tmdb.org/t/p/w500/';
+
 export const MovieDetails = () => {
   const [movie, setMovie] = useState(null);
   const { movieId } = useParams();
@@ -15,30 +16,7 @@ export const MovieDetails = () => {
     movie && (
       <>
         <ButtonGoBack location={location} />
-        <img
-          src={
-            movie.poster_path
-              ? `${imageUrl}${movie.poster_path}`
-              : 'https://e7.pngegg.com/pngimages/84/530/png-clipart-film-clapperboard-computer-icons-cinema-shows-miscellaneous-angle.png'
-          }
-          alt={movie.title}
-          width="300"
-        />
-        <h1>{movie.title}</h1>
-        <p>User Score: {movie.vote_average * 10}%</p>
-        <h2>Overview</h2>
-        <p>{movie.overview}</p>
-        {<h3>Genres</h3>}
-        {<p>{movie.genres.map(genre => genre.name).join(' ')}</p>}
-        <h3>Additional information</h3>
-        <ul>
-          <li>
-            <NavLink to="cast">Cast</NavLink>
-          </li>
-          <li>
-            <NavLink to="reviews">Reviews</NavLink>
-          </li>
-        </ul>
+        <MovieInfo movie={movie} />
         <Outlet />
       </>
     )
