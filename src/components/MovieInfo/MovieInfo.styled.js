ee1fb9934f8eda101ae9512270337637893bb5a1
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-12-27 19:16:14 +0300

    w

--- src/components/MovieInfo/MovieInfo.styled.js
+++ src/components/MovieInfo/MovieInfo.styled.js
@@ -8,6 +8,7 @@ export const Container = styled.div`
 export const Img = styled.img`
   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  width: 320px;
   border-radius: 5px;
 `;
 export const BoxInfo = styled.div`
